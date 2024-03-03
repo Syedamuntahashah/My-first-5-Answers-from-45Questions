2ba3923cd36261fe90aa503848163d7cb5d0af4c
Syedamuntahashah <161372867+Syedamuntahashah@users.noreply.github.com>
2024-03-03 17:43:12 +0500

    Add files via upload
    My Answers (Assignment)_

---
+++ code.ts
@@ -0,0 +1,31 @@
+// Question 1:
+
+let myName: string = 'Muntaha Shah';
+
+console.log(`hello ${myName }, would you like to learn some typeScript ?`);
+
+
+// Question 2:
+let personsName: string = 'Muntahashah';
+
+console.log(personsName. toUpperCase());
+console.log(personsName. toLowerCase());
+
+
+// Question 3:
+
+console.log('"Bill Gates once said,"Its fine to celebrate success but it is more important to heed the lessons of failure."');
+
+// Question 4:
+let famouse_person: string = "Bill Gates";
+let message:string = `${famouse_person} once said, "Its fine to celebrate success but it is more important to heed the lessons of failure." `
+
+console.log(message);
+
+// Question 5:
+
+let name1: string = "\t\n Muntaha \t\n";
+console.log(name1);
+
+
+
